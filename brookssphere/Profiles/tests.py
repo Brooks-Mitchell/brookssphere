40b433e56d2514b557843f32f71c34bf6fe331cf
Bemly <evan93.mitchell@gmail.com>
2023-05-03 19:09:45 -0600

    add login and registration tests

--- brookssphere/Profiles/tests.py
+++ brookssphere/Profiles/tests.py
@@ -3,25 +3,32 @@ import django
 from django.test import TestCase
 from django.urls import reverse
 
+from django.test import Client
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
 # TODO: Configure your database in settings.py and sync before running tests.
 
-class SimpleTest(TestCase):
-    """Tests for the application views."""
 
+class SignupTest(TestCase):
+    
     # Django requires an explicit setup() when running tests in PTVS (Python Tools for Visual Studio)
     @classmethod
     def setUpClass(cls):
-        super(SimpleTest, cls).setUpClass()
+        super(SignupTest, cls).setUpClass()
+        user = User.objects.create_user('TestUser', 'test@testmail.com', 'TestPass')
         django.setup()
 
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_login(self):
 
+        # log in with the user we created in the setup above
+        self.client.login(username='TestUser', password='TestPass')
+        response = self.client.get('/login/', follow=True)
+        # set user to object retrieved from db where username = 'TestUser'
+        user = User.objects.get(username='TestUser')
+        self.assertEqual(response.context['user'].username, 'TestUser')
 
-#class SignupTest(TestCase):
-    
-#    def setUp()
-#    username
+    # TODO - second test not working with user created in setUpClass     
+    def test_profile_creation(self):
+        user = User.objects.create_user('TestUser2', 'test@testmail.com2', 'TestPass2')
+        self.assertEqual(user.profile.profile_name, 'name')
