760d0a2a0392aa89d18e8199da5f2c197fcd4303
Bemly <evan93.mitchell@gmail.com>
2023-05-04 15:57:01 -0600

    change Profile.date_of_birth model to default time

--- brookssphere/Profiles/models.py
+++ brookssphere/Profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import datetime
 
 
 class Profile(models.Model):
@@ -9,5 +10,5 @@ class Profile(models.Model):
     profile_name = models.CharField(max_length=80, default="name") 
     operations_performed = models.IntegerField(default = 0)
     # TODO - fix this to be MM/DD/YYYY
-    date_of_birth = models.DateTimeField(auto_now_add=True, blank=True)
+    date_of_birth = models.DateTimeField(default=datetime.datetime.now())
 
